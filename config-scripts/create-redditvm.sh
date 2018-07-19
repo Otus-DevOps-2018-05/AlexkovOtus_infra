cd758b8f1c7632816bf5a4b5922b7be8a013b786
Alexandr Kovalev <filip_filipov@mail.ru>
2018-07-19 20:51:58 +0300

    Очередная попытка

--- config-scripts/create-redditvm.sh
+++ config-scripts/create-redditvm.sh
@@ -1,6 +1,6 @@
 gcloud compute instances create reddit-app-2\
   --boot-disk-size=10GB \
-  --image-project reddit-base-1531156337 \
+  --image-project reddit-base-1532020071 \
   --machine-type=g1-small \
   --tags puma-server \
   --restart-on-failure
--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
+set -e
+set -x
+
+APP_DIR=${1:-$HOME}
+
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+
+sudo mv -v /tmp/myunit.service /etc/systemd/system/myunit.service
+sudo systemctl start myunit
+sudo systemctl enable myunit
