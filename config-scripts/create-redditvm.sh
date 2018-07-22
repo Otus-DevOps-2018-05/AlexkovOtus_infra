d20a4746aaa71ffed1dee68adbc04d936905bf6d
Alexandr Kovalev <filip_filipov@mail.ru>
2018-07-22 12:10:29 +0300

    Change image-project to image

--- config-scripts/create-redditvm.sh
+++ config-scripts/create-redditvm.sh
@@ -1,6 +1,6 @@
 gcloud compute instances create reddit-app-2\
-  --boot-disk-size=10GB \
-  --image-project reddit-base-1532020071 \
+  --boot-disk-size=20GB \
+  --image reddit-base-1532250037 \
   --machine-type=g1-small \
   --tags puma-server \
   --restart-on-failure
